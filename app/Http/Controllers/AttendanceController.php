8ee4cc5bfff19bd06cf1f7c46154a91539bd84c1
Hamzaqureshi401 <46222436+Hamzaqureshi401@users.noreply.github.com>
2023-01-19 08:46:41 +0500

    Revert "model added"
    This reverts commit 9f3dec0f5fef09af31f68323c841419de88983f9.

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -365,7 +365,7 @@ class AttendanceController extends Controller
      public function storeStudentMark(Request $request){
 
          
-            $SemCourseWeightage_ID       = SemesterCourseWeightage::where(['SemCourse_ID' => $request->SemCourses_ID , 'Type' => dd($SemCourseWeightage_ID);
+            $SemCourseWeightage_ID       = SemesterCourseWeightage::where(['SemCourse_ID' => $request->SemCourses_ID , 'Type' => $request->type])->with('WeightageDetail')->first()->ID;
             
         foreach($request->Enroll_ID as $key => $Enroll_ID ){
 
