987ba2701908da43990408df91200efa8a85e552
Hamzaqureshi401 <46222436+Hamzaqureshi401@users.noreply.github.com>
2023-01-27 10:36:41 +0500

    se

--- resources/views/layouts/app_new.blade.php
+++ resources/views/layouts/app_new.blade.php
@@ -235,7 +235,7 @@
 
         <!-- star main content  -->
         @yield('content')
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/css/select2.min.css" rel="stylesheet"/>
+        
         
 
 
