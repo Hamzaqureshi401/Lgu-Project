65d1769dfe63e30e5f3d589e47abf2da35fe05e7
Hamzaqureshi401 <46222436+Hamzaqureshi401@users.noreply.github.com>
2023-06-25 22:14:27 -0700

    all

--- resources/views/Challans/allChallans.blade.php
+++ resources/views/Challans/allChallans.blade.php
@@ -46,7 +46,7 @@
                               <div class="card-body">
                                 <!-- only change id -->
                                
-                                <button type="button" class="btn btn-primary gt-data" data-toggle="modal" data-id="{{ $challan->ID }}" data-target="#exampleModal"><i class="far fa-edit"></i> {{ $modalTitle }}</button>
+                                <!-- <button type="button" class="btn btn-primary gt-data" data-toggle="modal" data-id="{{ $challan->ID }}" data-target="#exampleModal"><i class="far fa-edit"></i> {{ $modalTitle }}</button> -->
                                 
                               </div>
                             </td>
