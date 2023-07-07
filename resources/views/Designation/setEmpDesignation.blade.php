115b1bbb00ec665b10bcc039b588ccde1405cc61
hamzaQureshi4011 <125427428+hamzaQureshi4011@users.noreply.github.com>
2023-07-07 13:14:56 +0500

    select 2

--- resources/views/Designation/setEmpDesignation.blade.php
+++ resources/views/Designation/setEmpDesignation.blade.php
@@ -11,7 +11,7 @@
 
             <div class="form-group col-6">
                 <label>Designation</label>
-                <select class="form-control custom-select border border-1 rounded border-dark" name="Des_ID" required>
+                <select class="form-control custom-select border border-1 rounded border-dark select2" name="Des_ID" required>
                     @foreach ($designations as $designation)
                         <option value="{{ $designation->ID }}">{{ $designation->Designation }}</option>
                     @endforeach
@@ -20,7 +20,7 @@
 
             <div class="form-group col-6">
                 <label>Employee Name</label>
-                <select class="form-control custom-select border border-1 rounded border-dark" name="Emp_ID" required>
+                <select class="form-control custom-select border border-1 rounded border-dark select2" name="Emp_ID" required>
                     @foreach ($employees as $employee)
                         <option value="{{ $employee->ID }}">{{ $employee->Emp_FirstName }}{{ $employee->Emp_LastName }}
                         </option>
